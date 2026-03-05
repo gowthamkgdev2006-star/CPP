fdf70a79c041295b7a6c85ffab94a4b8785a2813
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-03-06 00:44:09 +0530

    Implement sum of even and product of odd numbers

--- EvenSumOddProduct.cpp
+++ EvenSumOddProduct.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+int sum = 0;
+int pro = 1;
+vector<int> demo = {2, 4, 3, 6, 1, 9, 8, 0, 8, 5, 7, 3, 3};
+for(int i = 0; i < demo.size(); i++)
+{
+  if(demo[i] % 2 == 0)
+  {
+    sum = sum + demo[i];
+  } 
+  else if(demo[i] % 2 == 1)
+  {
+     pro = pro * demo[i];
+  }
+}
+ cout << "The sum of even numbers: " << sum << "\n";
+ cout << "The product of odd numbers: " << pro << "\n";
+
+return 0;
+}
+
+
+
 
