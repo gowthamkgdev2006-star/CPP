fdb463c1bf597d39d451c70c2837bdf65a2d0f22
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-04 00:46:59 +0530

    Add Hello World program with user input

---
+++ Basics/01_helloWorld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Print
+    cout << "Hello World!" << endl;
+
+    // Declare a variable
+    string name;
+
+    // Get input
+    cout << "Enter your name: ";
+    cin >> name;
+
+    // Print the input
+    cout << "Hello, " << name << "!" << endl;
+
+    return 0;
+}
