809d17e840eca58976e625a4d1ab421f9397a78f
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-04 00:40:48 +0530

    Create constants.cpp

---
+++ Basics/constants.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //const vales can't be changed after initialization
+    const int AGE = 20;
+
+    //AGE = 21;   // ❌ Error
+
+    cout << AGE;
+
+    return 0;
+}
