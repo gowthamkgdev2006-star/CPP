2c50e22c7152f0ed6632e96bdfe6ae85b44a50b1
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-04 01:32:47 +0530

    Update 01_helloWorld.cpp

--- Basics/01_helloWorld.cpp
+++ Basics/01_helloWorld.cpp
@@ -1,21 +1,10 @@
+//basic structure of a C++
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main()
 {
-    // Print
-    cout << "Hello World!" << endl;
-
-    // Declare a variable
-    string name;
-
-    // Get input
-    cout << "Enter your name: ";
-    cin >> name;
-
-    // Print the input
-    cout << "Hello, " << name << "!" << endl;
+    cout << "Hello, World!" << endl;
 
     return 0;
 }
