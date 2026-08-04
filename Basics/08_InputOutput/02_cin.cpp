fe823c67a7e9316ecdaeb9d5070fb760880f424d
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-04 23:30:14 +0530

    Create 02_cin.cpp

---
+++ Basics/08_InputOutput/02_cin.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name; // to get user input
+
+    cout << "Hello, " << name << "!" << endl; 
+
+   return 0;
+}
