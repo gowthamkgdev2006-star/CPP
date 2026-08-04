29011dbd9c234eccc79b0ba9d8ceae2c9390091a
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-04 23:28:30 +0530

    Create 01_cout.cpp

---
+++ Basics/08_InputOutput/01_cout.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name = "Gowtham K";
+
+    cout << "Hello, " << name << "!" << endl; // Print statement
+
+    return 0;
+}
