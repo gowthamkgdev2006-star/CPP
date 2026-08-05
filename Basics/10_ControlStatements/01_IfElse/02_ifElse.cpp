bee04939d20c4cb2eb8f00ec27d0c5734daae51d
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-05 23:55:20 +0530

    Create  02_ifElse.cpp

---
+++ Basics/10_ControlStatements/01_IfElse/02_ifElse.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    // Executes only if the condition is true.
+    if(a < 5)
+    {
+        cout << "a is less than 5" << endl;
+    }
+
+    // Executes only if the condition is false.
+    else
+    {
+        cout << "a is not less than 5" << endl;
+    }
+    
+    return 0;
+}
