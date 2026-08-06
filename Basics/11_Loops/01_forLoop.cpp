9e2a24d28691edb688e556556f67e2c8e81d656d
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-06 16:53:44 +0530

    Create 01_forLoop.cpp

---
+++ Basics/11_Loops/01_forLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+
+    // Loop to print numbers from 1 to n
+    for(int i = 1; i <= n; i++) 
+    {
+        cout << i <<endl;
+    }
+
+    return 0;
+}
