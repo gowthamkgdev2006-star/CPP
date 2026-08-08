5619402fd826f3ba662cbd641d20bab0d9ae5ea3
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-08 23:37:01 +0530

    Change output format for array element access

--- Basics/14_Arrays/01_1DArray/01_arraySyntax.cpp
+++ Basics/14_Arrays/01_1DArray/01_arraySyntax.cpp
@@ -6,11 +6,11 @@ int main()
     int arr[5] = {1, 2, 3, 4, 5};
 
     // Accessing array elements using index
-    cout << "First element: " << arr[0] << endl;
-    cout << "Second element: " << arr[1] << endl;
-    cout << "Third element: " << arr[2] << endl;
-    cout << "Fourth element: " << arr[3] << endl;
-    cout << "Fifth element: " << arr[4] << endl;
-
+    cout << "Element at index " << 0 << ": " << arr[0] << endl;
+    cout << "Element at index " << 1 << ": " << arr[1] << endl;
+    cout << "Element at index " << 2 << ": " << arr[2] << endl;
+    cout << "Element at index " << 3 << ": " << arr[3] << endl;
+    cout << "Element at index " << 4 << ": " << arr[4] << endl;
+    
     return 0;
 }
