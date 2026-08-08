a996ef489546bb47fc5c29a02bd6507a47e3019d
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-08 23:14:32 +0530

    Create 02_arrayInputOutput.cpp

---
+++ Basics/14_Arrays/02_arrayInputOutput.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    cout << "Enter 5 elements: ";
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+     
+    cout << "First element: " << arr[0] << endl;
+    cout << "Second element: " << arr[1] << endl;
+    cout << "Third element: " << arr[2] << endl;
+    cout << "Fourth element: " << arr[3] << endl;
+    cout << "Fifth element: " << arr[4] << endl;
+    return 0;
+}
