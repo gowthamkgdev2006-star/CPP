66651f2baac92810669b17142b6ddbe311a371c1
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-09 01:35:48 +0530

    Simplify array element access output format

--- Basics/14_Arrays/01_1DArray/01_arraySyntax.cpp
+++ Basics/14_Arrays/01_1DArray/01_arraySyntax.cpp
@@ -6,11 +6,11 @@ int main()
     int arr[5] = {1, 2, 3, 4, 5};
 
     // Accessing array elements using index
-    cout << "Element at index " << 0 << ": " << arr[0] << endl;
-    cout << "Element at index " << 1 << ": " << arr[1] << endl;
-    cout << "Element at index " << 2 << ": " << arr[2] << endl;
-    cout << "Element at index " << 3 << ": " << arr[3] << endl;
-    cout << "Element at index " << 4 << ": " << arr[4] << endl;
+    cout << "Element at [0]: " << arr[0] << endl;
+    cout << "Element at [1]: " << arr[1] << endl;
+    cout << "Element at [2]: " << arr[2] << endl;
+    cout << "Element at [3]: " << arr[3] << endl;
+    cout << "Element at [4]: " << arr[4] << endl;
     
     return 0;
 }
