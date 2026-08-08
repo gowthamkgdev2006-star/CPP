f92da6282c22ec262051cf82911ab2e930aaa18c
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-08 23:08:22 +0530

    Create 01_arraySyntax.cpp

---
+++ Basics/14_Arrays/01_arraySyntax.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{         //Index 0  1  2  3  4
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    // Accessing array elements using index
+    cout << "First element: " << arr[0] << endl;
+    cout << "Second element: " << arr[1] << endl;
+    cout << "Third element: " << arr[2] << endl;
+    cout << "Fourth element: " << arr[3] << endl;
+    cout << "Fifth element: " << arr[4] << endl;
+
+    return 0;
+}
