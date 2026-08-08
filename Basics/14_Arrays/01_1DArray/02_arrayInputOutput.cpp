92509de148d64f97053734d5a7d1e7431c0c43b6
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-08 23:35:36 +0530

    Update 02_arrayInputOutput.cpp

--- Basics/14_Arrays/01_1DArray/02_arrayInputOutput.cpp
+++ Basics/14_Arrays/01_1DArray/02_arrayInputOutput.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main()
 {
     int arr[5];
-    cout << "Enter 5 elements: ";
+    cout << "Enter 5 elements: " << endl;
     cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
      
-    cout << "First element: " << arr[0] << endl;
-    cout << "Second element: " << arr[1] << endl;
-    cout << "Third element: " << arr[2] << endl;
-    cout << "Fourth element: " << arr[3] << endl;
-    cout << "Fifth element: " << arr[4] << endl;
+    cout << "Element at index " << 0 << ": " << arr[0] << endl;
+    cout << "Element at index " << 1 << ": " << arr[1] << endl;
+    cout << "Element at index " << 2 << ": " << arr[2] << endl;
+    cout << "Element at index " << 3 << ": " << arr[3] << endl;
+    cout << "Element at index " << 4 << ": " << arr[4] << endl;
+    
     return 0;
 }
