9d6d82f4e77dc60058c18779a8a578e072578328
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-09 00:28:36 +0530

    Create 02_arrayInputOutput.cpp

---
+++ Basics/14_Arrays/02_2DArray/01_arraySyntax.cpp02_2DArray/02_arrayInputOutput.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[2][3];
+     // Input elements into the 2D array
+    cout << "Enter 6 elements: " << endl;
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+     // Output elements of the 2D array
+    cout << "The elements of the 2D array are: " << endl;
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
