564b61084ef89678e91e4e0903140a7b75f72164
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-09 20:36:38 +0530

    Create 03_stringLength.cpp

---
+++ Basics/15_Strings/03_stringLength.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string word = "Hello World";
+    
+    cout << "Word: " << word << endl;
+    
+    //space is also counted as a character
+    cout << "Length: " << word.length() << endl;
+    
+    return 0;
+}
