54e45fe1202f35ca233fc7fad94cdd2ead1de48b
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-09 20:40:46 +0530

    Create 04_stringAccess.cpp

---
+++ Basics/15_Strings/04_stringAccess.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string word = "Hello";
+
+    // Accessing individual characters using index
+    cout << "First character: " << word[0] << endl;
+    cout << "Second character: " << word[1] << endl;
+    cout << "Third character: " << word[2] << endl;
+    cout << "Fourth character: " << word[3] << endl;
+    cout << "Fifth character: " << word[4] << endl;
+
+    return 0;
+}
