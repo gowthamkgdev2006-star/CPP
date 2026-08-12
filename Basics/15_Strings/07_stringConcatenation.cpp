322e31e10e3dad35ca3283be49fb511b2f8528e0
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-08-12 18:59:22 +0530

    Add string concatenation example in C++

---
+++ Basics/15_Strings/07_stringConcatenation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string firstName = "Gowtham";
+    string lastName = "K";
+
+    //Concatenation = joining strings together.
+    string fullName = firstName + " " + lastName; // Concatenating strings
+
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
